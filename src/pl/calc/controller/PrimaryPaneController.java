a9d25ef91df34d111aa15ea3702f16519b68d05d
Tata <m_rybak@wp.pl>
2019-05-04 20:38:54 +0200

    04.05.2019 - Separate Verify & Parser methods.

--- src/pl/calc/controller/PrimaryPaneController.java
+++ src/pl/calc/controller/PrimaryPaneController.java
@@ -7,6 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyEvent;
 import pl.calc.main.FieldCalculate;
+import pl.calc.main.Verify;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,6 +24,8 @@ public class PrimaryPaneController implements Initializable {
     private Button exitButton;
 
     FieldCalculate calculate = new FieldCalculate();
+    Verify verify = new Verify();
+
     String[] pays = new String[8];
 
     @Override
@@ -43,7 +46,7 @@ public class PrimaryPaneController implements Initializable {
         bonusPaneController.getBonusTextField().setOnKeyTyped(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                calculate.doubleValueVerify(bonusPaneController.getBonusTextField());
+                verify.doubleValueVerify(bonusPaneController.getBonusTextField());
                 calculate.factor(bonusPaneController.getPaymentTextField(),
                         bonusPaneController.getProfit19TextField());
                 calculate.factor(bonusPaneController.getPaymentTextField(),
@@ -71,7 +74,7 @@ public class PrimaryPaneController implements Initializable {
         bonusPaneController.getPaymentTextField().setOnKeyTyped(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                calculate.integerValueVerify(bonusPaneController.getPaymentTextField());
+                verify.integerValueVerify(bonusPaneController.getPaymentTextField());
                 calculate.factor(bonusPaneController.getPaymentTextField(),
                         bonusPaneController.getProfit19TextField());
                 calculate.factor(bonusPaneController.getPaymentTextField(),
@@ -83,7 +86,7 @@ public class PrimaryPaneController implements Initializable {
         bonusPaneController.getPointsTextField().setOnKeyTyped(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                calculate.integerValueVerify(bonusPaneController.getPointsTextField());
+                verify.integerValueVerify(bonusPaneController.getPointsTextField());
                 pays[0] = bonusPaneController.getPointsTextField().getText();
             }
         });
@@ -91,12 +94,12 @@ public class PrimaryPaneController implements Initializable {
         positionPaneController.getPayment1TextField().setOnKeyTyped(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                calculate.integerValueVerify(positionPaneController.getPayment1TextField());
+                verify.integerValueVerify(positionPaneController.getPayment1TextField());
                 pays[1] = positionPaneController.getPayment1TextField().getText();
-                pays[1] = calculate.pointsVerify(pays,
+                pays[1] = verify.pointsVerify(pays,
                                 positionPaneController.getPayment1TextField(),
                                 bonusPaneController.getPointsTextField());
-                calculate.checkGuaranteedPosition(pays,
+                verify.checkGuaranteedPosition(pays,
                         positionPaneController.getPayment1TextField(),
                         positionPaneController.getPayment2TextField()
                 );
@@ -109,13 +112,13 @@ public class PrimaryPaneController implements Initializable {
         positionPaneController.getPayment2TextField().setOnKeyTyped(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                calculate.integerValueVerify(positionPaneController.getPayment2TextField());
+                verify.integerValueVerify(positionPaneController.getPayment2TextField());
 
                 pays[2] = positionPaneController.getPayment2TextField().getText();
-                pays[2] = calculate.pointsVerify(pays,
+                pays[2] = verify.pointsVerify(pays,
                         positionPaneController.getPayment2TextField(),
                         positionPaneController.getPayment1TextField());
-                calculate.checkGuaranteedPosition(pays,
+                verify.checkGuaranteedPosition(pays,
                         positionPaneController.getPayment2TextField(),
                         positionPaneController.getPayment3TextField()
                 );
@@ -128,12 +131,12 @@ public class PrimaryPaneController implements Initializable {
         positionPaneController.getPayment3TextField().setOnKeyTyped(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                calculate.integerValueVerify(positionPaneController.getPayment3TextField());
+                verify.integerValueVerify(positionPaneController.getPayment3TextField());
                 pays[3] = positionPaneController.getPayment3TextField().getText();
-                pays[3] = calculate.pointsVerify(pays,
+                pays[3] = verify.pointsVerify(pays,
                         positionPaneController.getPayment3TextField(),
                         positionPaneController.getPayment2TextField());
-                calculate.checkGuaranteedPosition(pays,
+                verify.checkGuaranteedPosition(pays,
                         positionPaneController.getPayment3TextField(),
                         positionPaneController.getPayment4TextField()
                 );
@@ -146,12 +149,12 @@ public class PrimaryPaneController implements Initializable {
         positionPaneController.getPayment4TextField().setOnKeyTyped(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                calculate.integerValueVerify(positionPaneController.getPayment4TextField());
+                verify.integerValueVerify(positionPaneController.getPayment4TextField());
                 pays[4] = positionPaneController.getPayment4TextField().getText();
-                pays[4] = calculate.pointsVerify(pays,
+                pays[4] = verify.pointsVerify(pays,
                         positionPaneController.getPayment4TextField(),
                         positionPaneController.getPayment3TextField());
-                calculate.checkGuaranteedPosition(pays,
+                verify.checkGuaranteedPosition(pays,
                         positionPaneController.getPayment4TextField(),
                         positionPaneController.getPayment5TextField()
                 );
@@ -164,12 +167,12 @@ public class PrimaryPaneController implements Initializable {
         positionPaneController.getPayment5TextField().setOnKeyTyped(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                calculate.integerValueVerify(positionPaneController.getPayment5TextField());
+                verify.integerValueVerify(positionPaneController.getPayment5TextField());
                 pays[5] = positionPaneController.getPayment5TextField().getText();
-                pays[5] = calculate.pointsVerify(pays,
+                pays[5] = verify.pointsVerify(pays,
                         positionPaneController.getPayment5TextField(),
                         positionPaneController.getPayment4TextField());
-                calculate.checkGuaranteedPosition(pays,
+                verify.checkGuaranteedPosition(pays,
                         positionPaneController.getPayment5TextField(),
                         positionPaneController.getPayment6TextField()
                 );
@@ -182,12 +185,12 @@ public class PrimaryPaneController implements Initializable {
         positionPaneController.getPayment6TextField().setOnKeyTyped(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                calculate.integerValueVerify(positionPaneController.getPayment6TextField());
+                verify.integerValueVerify(positionPaneController.getPayment6TextField());
                 pays[6] = positionPaneController.getPayment6TextField().getText();
-                pays[6] = calculate.pointsVerify(pays,
+                pays[6] = verify.pointsVerify(pays,
                         positionPaneController.getPayment6TextField(),
                         positionPaneController.getPayment5TextField());
-                calculate.checkGuaranteedPosition(pays,
+                verify.checkGuaranteedPosition(pays,
                         positionPaneController.getPayment6TextField(),
                         positionPaneController.getRestTextField()
                 );
@@ -197,9 +200,9 @@ public class PrimaryPaneController implements Initializable {
         positionPaneController.getRestTextField().setOnKeyTyped(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent event) {
-                calculate.integerValueVerify(positionPaneController.getRestTextField());
+                verify.integerValueVerify(positionPaneController.getRestTextField());
                 pays[7] = positionPaneController.getRestTextField().getText();
-                pays[7] = calculate.pointsVerify(pays,
+                pays[7] = verify.pointsVerify(pays,
                         positionPaneController.getRestTextField(),
                         positionPaneController.getPayment6TextField());
             }
